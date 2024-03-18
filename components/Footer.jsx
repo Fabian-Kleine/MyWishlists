1574e05ab7dff0c0ed02ed9ce8077948a0d585f6
R6-Creative-Hub <96775695+Fabian-Kleine@users.noreply.github.com>
2024-03-18 23:22:19 +0100

    updated Footer

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,9 +1,15 @@
+import { Github, Globe } from "lucide-react";
+
 export default function Footer() {
     return (
-        <footer className="footer footer-center p-4 bg-base-300 text-base-content">
-            <aside>
+        <footer className="footer items-center p-4 bg-neutral text-neutral-content">
+            <aside className="items-center grid-flow-col">
                 <p>Copyright © 2024 - Made by <a className="underline font-bold" href="https://fabian-kleine.dev/" target="_blank">Fabian Kleine</a></p>
             </aside>
+            <nav className="grid-flow-col gap-4 md:place-self-center md:justify-self-end">
+            <a href="https://fabian-kleine.dev/" target="_blank"><Globe /></a>
+                <a href="https://github.com/Fabian-Kleine/wishlist" target="_blank"><Github /></a>
+            </nav>
         </footer>
     )
 }
