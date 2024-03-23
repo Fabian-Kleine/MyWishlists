509a835dd7d35db4d78fd74af698b229b11b81e0
R6-Creative-Hub <96775695+Fabian-Kleine@users.noreply.github.com>
2024-03-23 18:21:58 +0100

    fixed: user id cookie resetting

--- components/UpdateUserCookie.jsx
+++ components/UpdateUserCookie.jsx
@@ -6,7 +6,7 @@ import { supabase } from "@/utils/supabase";
 export default function UpdateUserCookie() {
     useEffect(() => {
         const { data: { subscription } } = supabase.auth.onAuthStateChange((event, session) => {
-            if (event == "SIGNED_OUT" || event == "INITIAL_SESSION") {
+            if (event == "SIGNED_OUT" || (event == "INITIAL_SESSION" && !session)) {
                 fetchUserId(null);
                 return;
             }
