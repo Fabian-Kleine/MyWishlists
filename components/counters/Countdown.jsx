203306c96163aa0483e49e66db2f3e78b2109610
R6-Creative-Hub <96775695+Fabian-Kleine@users.noreply.github.com>
2024-03-24 18:30:50 +0100

    added Countdown component

---
+++ components/counters/Countdown.jsx
@@ -0,0 +1,67 @@
+"use client"
+import { useState, useEffect } from "react"
+
+export default function Countdown({ date }) {
+    const calculateTimeLeft = () => {
+        const difference = +new Date(date) - +new Date();
+        console.log(difference)
+        let timeLeft = {};
+
+        if (difference > 0) {
+            timeLeft = {
+                months: Math.floor(difference / (1000 * 60 * 60 * 24 * 30.44)),
+                days: Math.floor((difference / (1000 * 60 * 60 * 24)) % 30.44),
+                hours: Math.floor((difference / (1000 * 60 * 60)) % 24),
+                minutes: Math.floor((difference / 1000 / 60) % 60),
+                seconds: Math.floor((difference / 1000) % 60)
+            };
+        }
+
+        return timeLeft;
+    };
+
+    const [timeLeft, setTimeLeft] = useState(calculateTimeLeft());
+
+    useEffect(() => {
+        const timer = setTimeout(() => {
+            setTimeLeft(calculateTimeLeft());
+        }, 1000);
+
+        return () => clearTimeout(timer);
+    });
+
+    return (
+        <div className="grid grid-flow-col gap-5 text-center auto-cols-max w-fit">
+            <div className="flex flex-col">
+                <span className="countdown font-mono text-5xl">
+                    <span style={{ "--value": timeLeft.months }}></span>
+                </span>
+                months
+            </div>
+            <div className="flex flex-col">
+                <span className="countdown font-mono text-5xl">
+                    <span style={{ "--value": timeLeft.days }}></span>
+                </span>
+                days
+            </div>
+            <div className="flex flex-col">
+                <span className="countdown font-mono text-5xl">
+                    <span style={{ "--value": timeLeft.hours }}></span>
+                </span>
+                hours
+            </div>
+            <div className="flex flex-col">
+                <span className="countdown font-mono text-5xl">
+                    <span style={{ "--value": timeLeft.minutes }}></span>
+                </span>
+                min
+            </div>
+            <div className="flex flex-col">
+                <span className="countdown font-mono text-5xl">
+                    <span style={{ "--value": timeLeft.seconds }}></span>
+                </span>
+                sec
+            </div>
+        </div>
+    )
+}
