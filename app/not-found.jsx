62f039a7d931e3576d1a80113909c023b70e40f3
R6-Creative-Hub <96775695+Fabian-Kleine@users.noreply.github.com>
2024-03-31 15:17:26 +0200

    feature: 404 page

---
+++ app/not-found.jsx
@@ -0,0 +1,21 @@
+import Link from "next/link"
+import Image from "next/image"
+import { Home } from "lucide-react"
+
+export default function NotFoundPage() {
+    return (
+        <main className="flex min-h-screen bg-hero-img bg-hero flex-col items-center justify-between mobile:px-24">
+            <div className="py-24 lg:px-24 w-full flex flex-col items-center gap-10">
+                <div>
+                    <Image src={'/404-Error.svg'} height={400} width={400} />
+                    <a className="link text-center block text-xs" href="https://storyset.com/web" target="_blank">Web illustrations by Storyset</a>
+                </div>
+                <div className="flex flex-col gap-2">
+                    <h1 className="text-3xl sm:text-4xl text-center font-bold ml-2">404 - Page Not Found!</h1>
+                    <p>The page you are looking for was moved or doesn't exist!</p>
+                </div>
+                <Link href={'/'} className="btn btn-primary btn-lg font-bold"><Home />Home</Link>
+            </div>
+        </main>
+    )
+}
