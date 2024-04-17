4debe9a129f7eb1ca0d4671bc055a0efac5118e9
R6-Creative-Hub <96775695+Fabian-Kleine@users.noreply.github.com>
2024-04-17 21:15:29 +0200

    fix: api not responding on failed scrape attemp

--- app/api/getProduct/route.js
+++ app/api/getProduct/route.js
@@ -17,31 +17,40 @@ export async function POST(req) {
     const domainname = url.hostname.replace(/(www\.)?/, '').split('.')[0];
 
     if (webshops.some(shop => shop.hostname === domainname)) {
-        const webshopdata = webshops.find((webshop) => {
-            return webshop.hostname == domainname;
-        });
-        const webpage = await axios.get(url);
-        const $ = cheerio.load(webpage.data);
-        let image = $(webshopdata.imageQuery).first().attr('src');
-        if (webshopdata.sourceImage) image = $(webshopdata.imageQuery).first().attr('srcset');
-        const title = !body.title ? $(webshopdata.titleQuery).first().text().trim() : body.title;
-        let price = !body.price ? $(webshopdata.priceQuery).first().text().trim() : body.price;
-        if (webshopdata.composedPrice && price != body.price) {
-            price = price + $(webshopdata.priceQuery2).first().text();
+        try {
+            const webshopdata = webshops.find((webshop) => {
+                return webshop.hostname == domainname;
+            });
+            const webpage = await axios.get(url);
+            const $ = cheerio.load(webpage.data);
+            let image = $(webshopdata.imageQuery).first().attr('src');
+            if (webshopdata.sourceImage) image = $(webshopdata.imageQuery).first().attr('srcset');
+            const title = !body.title ? $(webshopdata.titleQuery).first().text().trim() : body.title;
+            let price = !body.price ? $(webshopdata.priceQuery).first().text().trim() : body.price;
+            if (webshopdata.composedPrice && price != body.price) {
+                price = price + $(webshopdata.priceQuery2).first().text();
+            }
+            if (webshopdata.replacePriceText && price != body.price) {
+                price = price.replace(/[^0-9,]/g, '').trim();
+            }
+            if (webshopdata.affiliateID) {
+                url.hash = "";
+                url.search = `?tag=${webshopdata.affiliateID}`;
+            }
+            return NextResponse.json({
+                title,
+                image,
+                price,
+                link: url.href
+            });
+        } catch {
+            return NextResponse.json({
+                title: body.title,
+                price: body.price,
+                image: null,
+                link: body.link
+            });
         }
-        if (webshopdata.replacePriceText && price != body.price) {
-            price = price.replace(/[^0-9,]/g, '').trim();
-        }
-        if(webshopdata.affiliateID) {
-            url.hash = "";
-            url.search = `?tag=${webshopdata.affiliateID}`;
-        }
-        return NextResponse.json({
-            title,
-            image,
-            price,
-            link: url.href
-        });
     }
 
     return NextResponse.json({
