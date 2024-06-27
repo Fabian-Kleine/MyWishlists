86aa3c12ff182259c90fe4668998a607ca8fec73
R6-Creative-Hub <96775695+Fabian-Kleine@users.noreply.github.com>
2024-06-27 15:15:53 +0200

    fix: empty image bug

--- app/list/[listid]/page.jsx
+++ app/list/[listid]/page.jsx
@@ -54,7 +54,7 @@ export default async function Wishlist({ params: { listid } }) {
                     wishlist.products.map((product, index) =>
                         <div href={product.link} target="_blank" key={index} className="card card-compact w-96 bg-base-100 shadow-xl h-fit">
                             <figure className="h-[250px] flex justify-center items-center">
-                                {product.image != null ? (
+                                {product.image ? (
                                     <img className="object-cover" src={product.image} alt="Product Image" />
                                 ) : (
                                     <ImageOff height={90} width={90} className="object-cover" />
