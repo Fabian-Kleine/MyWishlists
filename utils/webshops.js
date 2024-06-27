8c9eaeb230198824ed1f3af49ecfe00bb584417c
R6-Creative-Hub <96775695+Fabian-Kleine@users.noreply.github.com>
2024-06-27 15:03:43 +0200

    chore: remove unused keys from webshops.js

--- utils/webshops.js
+++ utils/webshops.js
@@ -2,7 +2,6 @@ const webshops = [
     {
         hostname: "amazon",
         imageQuery: "#landingImage",
-        titleQuery: "#productTitle",
         composedPrice: true,
         priceQuery: ".a-price-whole",
         priceQuery2: ".a-price-fraction",
@@ -10,37 +9,26 @@ const webshops = [
     },
     {
         hostname: "ebay",
-        imageQuery: ".image img",
-        titleQuery: ".x-item-title__mainTitle",
         replacePriceText: true,
         priceQuery: ".x-price-primary",
     }, 
     {
         hostname: "kleinanzeigen",
-        imageQuery: "#viewad-image",
-        titleQuery: "#viewad-title",
         replacePriceText: true,
         priceQuery: "#viewad-price",
     },
     {
         hostname: "lego",
-        imageQuery: '[data-test="mediagallery-image-0"] source',
-        sourceImage: true,
-        titleQuery: '[data-test="product-overview-name"] .Markup__StyledMarkup-nc8x20-0.epIXnJ',
         replacePriceText: true,
         priceQuery: '[data-test="product-price"]'
     },
     {
         hostname: "zalando",
-        imageQuery: ".sDq_FX.lystZ1.FxZV-M._2Pvyxl.JT3_zV.EKabf7.mo6ZnF._1RurXL.mo6ZnF._7ZONEy",
-        titleQuery: ".EKabf7.R_QwOV",
         replacePriceText: true,
         priceQuery: ".sDq_FX._4sa1cA"
     },
     {
         hostname: "aboutyou",
-        imageQuery: '[data-testid="productImageView"]',
-        titleQuery: '[data-testid="productName"]',
         replacePriceText: true,
         priceQuery: '[data-testid="finalPrice"]'
     }
