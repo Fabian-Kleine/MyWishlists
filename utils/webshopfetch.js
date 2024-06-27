b4d89d33cc52171df2d4333de42786f32270f774
R6-Creative-Hub <96775695+Fabian-Kleine@users.noreply.github.com>
2024-06-27 15:03:20 +0200

    feat: improve webshopfetch

--- utils/webshopfetch.js
+++ utils/webshopfetch.js
@@ -3,27 +3,36 @@ import cheerio from "cheerio";
 import webshops from "./webshops";
 
 export default async function WebshopFetch(body) {
-    if (!body.link) {
-        return {
-            title: body.title,
-            price: body.price,
-            image: null,
-            link: body.link
-        };
+    let productData = {
+        title: body.title,
+        description: body.description || "",
+        price: body.price,
+        image: null,
+        link: body.link
     }
-    const url = new URL(body.link);
-    const domainname = url.hostname.replace(/(www\.)?/, '').split('.')[0];
 
-    if (webshops.some(shop => shop.hostname === domainname)) {
-        const webshopdata = webshops.find((webshop) => {
-            return webshop.hostname == domainname;
-        });
-        try {
-            const webpage = await axios.get(url);
-            const $ = cheerio.load(webpage.data);
-            let image = $(webshopdata.imageQuery).first().attr('src');
-            if (webshopdata.sourceImage) image = $(webshopdata.imageQuery).first().attr('srcset');
-            const title = !body.title ? $(webshopdata.titleQuery).first().text().trim() : body.title;
+    if (!body.link) return productData;
+
+    try {
+        const url = new URL(body.link);
+        const domainname = url.hostname.replace(/(www\.)?/, '').split('.')[0];
+
+        const webpage = await axios.get(url);
+        const $ = cheerio.load(webpage.data);
+
+        const metadata = {
+            title: $('meta[property="og:title"]').attr('content') || $('title').text() || '',
+            description: $('meta[name="description"]').attr('content') || $('meta[property="og:description"]').attr('content') || 'No description found',
+            image: $('meta[property="og:image"]').attr('content') || null,
+        };
+
+        productData = {...productData, ...metadata};
+
+        if (webshops.some(shop => shop.hostname === domainname)) {
+            const webshopdata = webshops.find((webshop) => {
+                return webshop.hostname == domainname;
+            });
+
             let price = !body.price ? $(webshopdata.priceQuery).first().text().trim() : body.price;
             if (webshopdata.composedPrice && price != body.price) {
                 price = price + $(webshopdata.priceQuery2).first().text();
@@ -31,34 +40,23 @@ export default async function WebshopFetch(body) {
             if (webshopdata.replacePriceText && price != body.price) {
                 price = price.replace(/[^0-9,]/g, '').trim();
             }
-            if (webshopdata.affiliateID) {
-                url.hash = "";
-                url.search = `?tag=${webshopdata.affiliateID}`;
-            }
-            return {
-                title,
-                image,
-                price,
-                link: url.href
-            };
-        } catch {
-            if (webshopdata.affiliateID) {
-                url.hash = "";
-                url.search = `?tag=${webshopdata.affiliateID}`;
-            }
-            return {
-                title: body.title,
-                price: body.price,
-                image: null,
-                link: url.href
-            };
-        }
-    }
 
-    return {
-        title: body.title,
-        price: body.price,
-        image: null,
-        link: body.link
-    };
+            productData.price = price;
+
+            if(webshopdata.imageQuery) {
+                const image = $(webshopdata.imageQuery).first().attr('src');
+                productData.image = image;
+            }
+    
+            if (webshopdata.affiliateID) {
+                url.hash = "";
+                url.search = `?tag=${webshopdata.affiliateID}`;
+                productData.link = url.href;
+            }
+        }
+    } catch (error) {
+        console.log(error)
+    } finally {
+        return productData;
+    }
 }
