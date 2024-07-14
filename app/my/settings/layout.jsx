6230e1b3a837cff362b8ea2bae4c806ef3094b76
R6-Creative-Hub <96775695+Fabian-Kleine@users.noreply.github.com>
2024-07-14 18:24:01 +0200

    fix: user data not loading on page refresh

--- app/my/settings/layout.jsx
+++ app/my/settings/layout.jsx
@@ -1,16 +1,11 @@
 import CheckLoggedIn from "@/utils/checkLoggedIn";
-import { Suspense } from "react";
 
 export default function Layout({ children }) {
     return (
         <main className="flex min-h-screen bg-hero-img bg-hero flex-col items-center justify-between mobile:px-24">
             <div className="py-24 lg:px-24 w-full flex flex-col items-center">
                 <h1 className="text-3xl sm:text-4xl text-center font-bold ml-2">Account Settings</h1>
-                <Suspense fallback={
-                    <span className="loading loading-spinner loading-lg mt-10"></span>
-                }>
-                    {children}
-                </Suspense>
+                {children}
             </div>
             <CheckLoggedIn redirectUrl={'/my/signin?unauthorized=1&redirectURL=%2Fmy%2Fsettings'} redirectOnValid={false} />
         </main>
--- app/my/settings/page.jsx
+++ app/my/settings/page.jsx
@@ -2,6 +2,7 @@
 import { supabase } from "@/utils/supabase";
 import ErrorModal, { ShowErrorModal } from "@/components/modals/ErrorModal";
 import SuccessModal, { ShowSuccessModal } from "@/components/modals/SuccessModal";
+import { useEffect, useState } from "react";
 
 async function getUserProfile() {
     try {
@@ -10,7 +11,7 @@ async function getUserProfile() {
         if (sessionError) {
             throw new Error(sessionError.message);
         }
-        
+
         const { data: profile, error } = await supabase
             .from("profiles")
             .select("username")
@@ -20,15 +21,34 @@ async function getUserProfile() {
         if (error) throw new Error(error);
 
         const user = { ...session.user, ...profile };
-        return user;
+        return { data: user, error: null };
     } catch (error) {
         console.error(error);
-        return {};
+        return { data: null, error };
     }
 }
 
-export default async function Settings() {
-    let user = await getUserProfile();
+export default function Settings() {
+    const [user, setUser] = useState([]);
+    const [isLoading, setIsLoading] = useState(false);
+    const [errorText, setErrorText] = useState("");
+
+    useEffect(() => {
+        async function loadUserProfile() {
+            setIsLoading(true);
+            const { data, error } = await getUserProfile();
+            if (error) {
+                setErrorText(error);
+                ShowErrorModal();
+                setIsLoading(false);
+                return;
+            }
+            setUser(data);
+            setIsLoading(false);
+        }
+
+        loadUserProfile();
+    }, []);
 
     async function handleSaveUser(event) {
         event.preventDefault();
@@ -38,15 +58,16 @@ export default async function Settings() {
                 .update({ username: event.target[0].value })
                 .eq("user_id", user.id);
 
-            user = { ...user, ...{ username: event.target[0].value } }
+            setUser({ ...user, ...{ username: event.target[0].value } })
 
             if (error) {
                 console.error(error);
+                setErrorText("An Error occured while saving your account data. Please try again later!")
                 ShowErrorModal();
                 return;
             }
         }
-        if(event.target[1].value) {
+        if (event.target[1].value) {
             const { error } = await supabase.auth.updateUser({
                 password: event.target[1].value
             });
@@ -64,28 +85,33 @@ export default async function Settings() {
     }
 
     return (
-        <div className="w-full lg:w-1/2 px-4">
-            <form onSubmit={handleSaveUser}>
-                <div className="flex flex-col gap-2">
-                    <label className="form-control w-full">
-                        <div className="label">
-                            <span className="label-text text-lg">Username</span>
+        <>
+            {isLoading ?
+                <span className="loading loading-spinner loading-lg mt-10"></span>
+                : <div className="w-full lg:w-1/2 px-4">
+                    <form onSubmit={handleSaveUser}>
+                        <div className="flex flex-col gap-2">
+                            <label className="form-control w-full">
+                                <div className="label">
+                                    <span className="label-text text-lg">Username</span>
+                                </div>
+                                <input defaultValue={user?.username} id="username" name="username" type="text" placeholder="Username" className="input input-bordered input-lg w-full" />
+                            </label>
+                            <label className="form-control w-full">
+                                <div className="label">
+                                    <span className="label-text">New Password</span>
+                                </div>
+                                <input id="newpassword" name="newpassword" type="password" placeholder="******" className="input input-bordered w-full" />
+                            </label>
+                            <div className="flex justify-end mt-5">
+                                <button className="btn btn-primary" type="submit">Save</button>
+                            </div>
                         </div>
-                        <input defaultValue={user?.username} id="username" name="username" type="text" placeholder="Username" className="input input-bordered input-lg w-full" />
-                    </label>
-                    <label className="form-control w-full">
-                        <div className="label">
-                            <span className="label-text">New Password</span>
-                        </div>
-                        <input id="newpassword" name="newpassword" type="password" placeholder="******" className="input input-bordered w-full" />
-                    </label>
-                    <div className="flex justify-end mt-5">
-                        <button className="btn btn-primary" type="submit">Save</button>
-                    </div>
+                    </form>
+                    <ErrorModal errorText={errorText} />
+                    <SuccessModal successText={"Successfully saved your account data!"} />
                 </div>
-            </form>
-            <ErrorModal errorText={"An Error occured while saving your account data. Please try again later!"} />
-            <SuccessModal successText={"Successfully saved your account data!"} />
-        </div>
+            }
+        </>
     )
 }
